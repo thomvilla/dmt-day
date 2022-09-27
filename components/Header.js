2b5e06fe07fd7077bd014a798b051a2b7c020343
Thomas Villaverde <thomasvillaverde16@gmail.com>
2022-09-26 23:49:58 -0300

    fix build

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import { Parallax } from 'react-scroll-parallax';
-
+import Link from "next/link"
 function Component(children) {
   return (
     <Parallax speed={10}>
@@ -11,12 +11,12 @@ export default function Header({ name }) {
   
   return (
     <header className="pb-12 ">   
-      <a href="/">
+      <Link href="/">
         <Parallax speed={-2} className="flex flex-col items-center justify-center w-full ">
           <img className='' src="https://i.postimg.cc/qvz1f2ym/descarga-1-removebg-preview.png" />
         </Parallax>
         <div className="m-4 text-3xl font-bold animate-pulse md:text-7xl" > DEMENTE DAY 20 NOV </div>
-      </a>
+      </Link>
       <p className="text-2xl text-center cursor-pointer dark:text-white">
         <div className="border collapse collapse-plus border-base-300 bg-base-100 rounded-box ">
           <input type="checkbox" /> 
